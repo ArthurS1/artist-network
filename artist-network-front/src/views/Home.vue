e4847f7fedcdc7b33bfe50183c9f4271cfcad5b6
Arthur Soulié <arthur.soulie@tuta.io>
2021-03-03 00:15:55 +0100

    Add basic post frontend

--- artist-network-front/src/views/Home.vue
+++ artist-network-front/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="home">
-        <b-navbar toggleable="lg" class="main_navbar">
+        <b-navbar :sticky="true" toggleable="lg" class="main_navbar">
             <img
                 class="logo mr-3"
                 alt="Vue logo"
@@ -35,18 +35,92 @@
         <b-modal v-model="showAccount" :hide-footer="true" :hide-header="true">
             <h3 class="title">Mon Compte</h3>
             <div class="d-flex flex-column mt-5">
-                <b-button pill variant="warning m-2" @click="handleLogout">Logout</b-button>
-                <b-button pill variant="danger m-2" @click="handleDelete">Delete account</b-button>
+                <b-button pill variant="warning m-2" @click="handleLogout"
+                    >Logout</b-button
+                >
+                <b-button pill variant="danger m-2" @click="handleDelete"
+                    >Delete account</b-button
+                >
             </div>
         </b-modal>
         <b-modal v-model="showPublish" :hide-footer="true" :hide-header="true">
             <h3 class="title">Publier</h3>
             <p class="text-center w-100 mt-5">aucune image sélectionnée</p>
             <div class="d-flex flex-column mt-2">
-                <b-button pill variant="light m-2">Sélectionner une image</b-button>
-                <b-button pill variant="primary m-2" :disabled="true">Publier</b-button>
+                <b-button pill variant="light m-2"
+                    >Sélectionner une image</b-button
+                >
+                <b-button pill variant="primary m-2" :disabled="true"
+                    >Publier</b-button
+                >
             </div>
         </b-modal>
+        <div class="d-flex flex-column align-items-center">
+            <div class="form-container d-flex flex-column p-lg-3 p-2 m-2">
+                <div class="">
+                    <div class="d-flex justify-content-between">
+                        <p>ArthurS</p>
+                        <p>02/03/21 22:11</p>
+                    </div>
+                    <h3>Titre.</h3>
+                    <div class="d-flex flex-column align-content-center">
+                        <b-img
+                            rounded
+                            class="my-2"
+                            src="https://picsum.photos/250/250/?image=54"
+                        ></b-img>
+                    </div>
+                    <div class="d-flex align-content-center">
+                        <div class="mr-2">
+                            <b-icon-suit-heart-fill
+                                class="mr-2"
+                                variant="danger"
+                            ></b-icon-suit-heart-fill>
+                            <small>20</small>
+                        </div>
+                        <div>
+                            <b-icon-binoculars
+                                class="mr-2"
+                                variant="primary"
+                            ></b-icon-binoculars>
+                            <small>20</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="form-container d-flex flex-column p-lg-3 p-2 m-2">
+                <div class="">
+                    <div class="d-flex justify-content-between">
+                        <p>ArthurS</p>
+                        <p>02/03/21 22:11</p>
+                    </div>
+                    <h3>Titre.</h3>
+                    <div class="d-flex flex-column align-content-center">
+                        <b-img
+                            rounded
+                            class="my-2"
+                            src="https://picsum.photos/250/250/?image=54"
+                        ></b-img>
+                    </div>
+                    <div class="d-flex align-content-center">
+                        <div class="mr-2">
+                            <b-icon-suit-heart-fill
+                                class="mr-2"
+                                variant="danger"
+                            ></b-icon-suit-heart-fill>
+                            <small>20</small>
+                        </div>
+                        <div>
+                            <b-icon-binoculars
+                                class="mr-2"
+                                variant="primary"
+                            ></b-icon-binoculars>
+                            <small>20</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -64,16 +138,12 @@ export default {
     },
     methods: {
         handleLogout() {
-            this.axios.get('logout')
-            .then((response) => {
-                if (response.data.logout == 'ok')
-                    this.$router.push('/login');
-            })
+            this.axios.get("logout").then((response) => {
+                if (response.data.logout == "ok") this.$router.push("/login");
+            });
         },
-        handleDelete() {
-
-        }
-    }
+        handleDelete() {},
+    },
 };
 </script>
 
@@ -92,5 +162,19 @@ export default {
 
 .main_navbar {
     background-color: white;
+}
+
+.form-container {
+    background-color: white;
+    border-radius: 0.5rem;
+    width: 35%;
+}
+
+@media (max-width: 1500px) {
+    .form-container {
+        background-color: white;
+        border-radius: 0.5rem;
+        width: 95%;
+    }
 }
 </style>
